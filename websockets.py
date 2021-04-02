bfa3519f5e0ddd0043b208741c5352624ed91c31
Maximilian Röhrle <4374065+edgale@users.noreply.github.com>
2021-04-02 08:16:13 +0200

    removed python script

--- websockets.py
+++
@@ -1,8 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://rancilio.local:81")
-ws.send("power")
-print("Sent")
-print("Receiving...")
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
